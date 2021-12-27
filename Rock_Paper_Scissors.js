f170c9a4b581ccbb805b5289cad4368e817585bd
darmijo15 <daniel.armijo@wsu.edu>
2021-12-27 09:05:03 -0800

    Function playRound() added, compares user play with computerPlay() output to determine a win and display a message.

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -7,4 +7,27 @@ function computerPlay() {
     } else {
     return 'scissors';
     }
-    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock crushess scissors.';
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper covers rock.';
+        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors cuts paper.';
+        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper covers rock.';
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors cuts paper.';
+        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock crushes scissors.';
+        } else {
+        return 'Draw! Play another round.';
+        }
+        }
+        
+const playerSelection = 'ROck';
+const computerSelection = computerPlay();
+        
+console.log(playRound(playerSelection, computerSelection));        
