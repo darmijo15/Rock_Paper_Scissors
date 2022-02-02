f4252c8a7ded9b271156b9031f129b87cc58e045
darmijo15 <daniel.armijo@wsu.edu>
2022-02-01 19:30:01 -0800

    UI added to display result message and declare a winner once a score of 5 has been reached.

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -12,6 +12,8 @@ function computerPlay() {
 let userScore = 0;
 let computerScore = 0;
 
+const resultDiv = document.querySelector('#result');
+
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', function(){playRound('rock', computerPlay())});
 
@@ -22,38 +24,44 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', function(){playRound('scissors', computerPlay())});
 
 function playRound(playerSelection, computerSelection) {
-    
+
+    const para = document.createElement('p');
+
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
             ++userScore;
-            console.log(computerSelection);
-            console.log(`You won this round! Rock crushes scissors. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You won this round! Rock crushes scissors. Score: ${userScore} - ${computerScore}`;
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             ++userScore;
-            console.log(computerSelection);
-            console.log(`You won this round! Paper covers rock. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You won this round! Paper covers rock. Score: ${userScore} - ${computerScore}`;
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
             ++userScore;
-            console.log(computerSelection);
-            console.log(`You won this round! Scissors cuts paper. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You won this round! Scissors cuts paper. Score: ${userScore} - ${computerScore}`;
         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
             ++computerScore;
-            console.log(computerSelection);
-            console.log(`You lost this round. Paper covers rock. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You lost this round. Paper covers rock. Score: ${userScore} - ${computerScore}`;
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
             ++computerScore;
-            console.log(computerSelection);
-            console.log(`You lost this round. Scissors cuts paper. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You lost this round. Scissors cuts paper. Score: ${userScore} - ${computerScore}`;
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
             ++computerScore;
-            console.log(computerSelection);
-            console.log(`You lost this round. Rock crushes scissors. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. You lost this round. Rock crushes scissors. Score: ${userScore} - ${computerScore}`;
         } else {
-            console.log(computerSelection);
-            console.log(`Draw, no winner this round. Score: ${userScore} - ${computerScore}`);
+            para.textContent = `Computer selected ${computerSelection}. Draw, no winner this round. Score: ${userScore} - ${computerScore}`;
         }
+
+    if (userScore === 5) {
+        para.textContent = `You win! Final score: ${userScore} - ${computerScore}. To play another round, select rock, paper, or scissors.`;
+        userScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        para.textContent = `You lost. Final score: ${userScore} - ${computerScore}. To play another round, select rock, paper, or scissors.`;
+        userScore = 0;
+        computerScore = 0;
     }
+    
+    resultDiv.appendChild(para);
 
-
+    }
 
 // function game() {
 //     for (let i = 0; i < 5; i++) {
